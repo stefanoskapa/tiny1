4ea48184dda197b0ff3b52880aa2b36f85fcead2
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2023-08-31 21:10:50 +0300

    remove tests

--- src/main/java/com/tiny1/model/HttpResponses.java
+++ src/main/java/com/tiny1/model/HttpResponses.java
@@ -8,7 +8,6 @@ public interface HttpResponses {
     String NOT_FOUND = "HTTP/1.0 404 NotFound";
     String INTERNAL_SERVER_ERROR = "HTTP/1.0 500 InternalServerError";
     String METHOD_NOT_ALLOWED = "HTTP/1.0 501 NotImplemented";
-
     String MOVED_PERMANENTLY = "HTTP/1.0 301 MovedPermanently";
 
 }
